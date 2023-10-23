90aa980e412f9f59322b065d0885848577781b41
muhammad morsy <muhammadmorsy5@gmail.com>
2023-10-23 15:34:02 -0700

    fix code

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,6 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
             num_item += 1
         except IndexError:
-            break
-        print()
-        return (num_item)
+            pass
+    print()
+    return (num_item)
