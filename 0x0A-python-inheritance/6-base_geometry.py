c4adc8b63e1a964742848f79243d05ccdcf07959
Muhammad <muhammadmorsy5@gmail.com>
2024-02-05 17:52:31 -0800

    Public instance method which is not implemented that raises an Exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+module that contain a class named BaseGeometry .
+"""
+
+
+class BaseGeometry():
+    """
+    a class that contain area() method.
+    """
+    def area(self):
+        """
+        area() Method which is not implemented.
+        it will raises an Exception with the message area() is not implemented.
+        """
+        raise Exception("area() is not implemented")
