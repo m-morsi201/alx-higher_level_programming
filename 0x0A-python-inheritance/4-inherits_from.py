4e305550f2fd47f5fc367d79283b54ff89e8969b
Muhammad <muhammadmorsy5@gmail.com>
2024-02-05 17:41:27 -0800

    fix pycodestyle for 4-inherits_from.py and make an empty class  named 'BaseGeometry'

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,7 +7,7 @@ module that Contains function that compares an object with an instance.
 def inherits_from(obj, a_class):
     """
      function that returns True if:
-        the object is an instance of a class that inherited from the specified class
+        the object is an instance of a class that inherited from the a class
      otherwise False.
     """
     return issubclass(type(obj), a_class) and type(obj) != a_class
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+module that contain an empty class.
+"""
+
+
+class BaseGeometry():
+    """
+    an empty class.
+    """
+    pass
