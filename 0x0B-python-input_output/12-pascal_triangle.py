b2d4cadfdc5faff2f4f38a14acf75e9f8933c222
Muhammad <muhammadmorsy5@gmail.com>
2024-02-06 17:39:13 -0800

    function  that returns a list of lists of integers representing the Pascal’s triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/pyhton3
+"""
+Module that define pascal_triangle() function.
+"""
+
+
+def pascal_triangle(n):
+    """
+    function that returns a list of lists of
+    integers representing the Pascal’s triangle of n:
+        1. Returns an empty list if n <= 0
+        2. n will be always an integer
+    """
+    if n <= 0:
+        return []
+
+    t = [[1]]
+    while len(t) != n:
+        tr = t[-1]
+        m = [1]
+        for item in range(len(tr) - 1):
+            m.append(tr[item] + tr[item + 1])
+        m.append(1)
+        t.append(m)
+    return t
