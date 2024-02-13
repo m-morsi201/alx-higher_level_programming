365f5b8c1e7478965e3970213a1caec22eec0e3c
Muhammad <muhammadmorsy5@gmail.com>
2024-02-12 16:29:30 -0800

    Update the class Base by adding the static method def from_json_string(json_string)

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -54,3 +54,13 @@ class Base():
 
         with open(fname, 'w') as f:
             f.write(ls)
+
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        Static Method that returns the list of the JSON string representation.
+        """
+        if json_string is None or len(json_string) == 0:
+            return []
+        return(json.loads(json_string))
