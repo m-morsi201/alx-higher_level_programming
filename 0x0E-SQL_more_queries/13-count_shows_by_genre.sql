4b25cd7181bd210c7b6fae7412545bf96399496d
Muhamad Morsy <muhammadmorsy5@gmail.com>
2024-03-13 17:14:55 -0700

    tasks 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,6 @@
 -- Script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter.
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows FROM tv_show_genre 
-	INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-	GROUP BY genre
-	ORDER BY number_of_shows DESC
+SELECT tv_genres.name AS `genre`, COUNT(*) AS `number_of_shows`
+	FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+	GROUP BY tv_genres.name
+	ORDER BY number_of_shows
+	DESC;
