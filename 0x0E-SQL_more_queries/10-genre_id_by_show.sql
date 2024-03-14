b4e53e59b9c68f8ce5bbbb80a90e093efd757fee
Muhamad Morsy <muhammadmorsy5@gmail.com>
2024-03-13 17:04:01 -0700

    tasks 13, 14 and edit 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,5 @@
 -- Script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
-SELECT cities.id, cities.name, states.name FROM cities
-INNER JOIN states ON cities.state_id=states.id ORDER BY cities.id
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+	INNER JOIN tv_show_genres
+	ON tv_shows.id = tv_show_genres.show_id
+	ORDER BY tv_shows.title, tv_show_genres.genre_id;
