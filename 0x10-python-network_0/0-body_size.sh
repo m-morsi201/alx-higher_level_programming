bf16a8948fb816ae5cb753949e685d91d04a8839
Muhamad Morsy <muhammadmorsy5@gmail.com>
2024-05-29 18:24:46 -0700

    README file

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+<<<<<<< HEAD
 # Bash script sends a request to that URL, and displays the size of the body of the response.
 curl -s "$1" | wc -c
+=======
+# Bash script that takes in a URL, sends a request to that URL,
+#	and displays the size of the body of the response.
+
+>>>>>>> all tasks
