b74d6a59243ec63f627a53426084a2cfc38dd5c8
Muhammad Morsy <muhammadmorsy5@gmail.com>
2024-06-24 21:34:06 -0700

    script that prints 3 lines: (like 1-multi_languages.js)

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// script that prints 3 lines: (like 1-multi_languages.js)
+const myList = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; myList[i]; i++) {
+  console.log(myList[i]);
+}
