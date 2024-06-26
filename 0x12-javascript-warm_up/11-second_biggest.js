1f2f11612b7512e5eb0d0af46f05489e1ef7450d
Muhammad Morsy <muhammadmorsy5@gmail.com>
2024-06-25 19:43:01 -0700

    edit code of task 11 to handel some cases

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
 // script that searches the second biggest integer in the list of arguments.
-const nums = process.argv.splice(2, process.argv.length).reverse();
-console.log(nums[1]);
+const nums = process.argv.splice(2, process.argv.length);
+function descendingOrder (arr) {
+  return (arr.sort().reverse());
+}
+const orderNums = descendingOrder(nums);
+console.log(orderNums[1]);
