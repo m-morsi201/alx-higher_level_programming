07f5470183a9066d73359337f09371fb0bc0cfd8
Muhammad Morsy <muhammadmorsy5@gmail.com>
2024-06-25 21:10:29 -0700

    hadel errors in checker

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,15 @@
 #!/usr/bin/node
 // script that searches the second biggest integer in the list of arguments.
-const nums = process.argv.splice(2, process.argv.length);
-function descendingOrder (arr) {
-  return (arr.sort().reverse());
+const args = process.argv.splice(2, process.argv.length);
+const ints = [];
+for (let i = 0; i < args.length; i++) {
+  ints.push(parseInt(args[i]));
 }
-const orderNums = descendingOrder(nums);
-if (orderNums <= 1) {
+const descInts = ints.sort(function (a, b) {
+  return (b - a);
+});
+if (descInts.length <= 1) {
   console.log('0');
 } else {
-  console.log(orderNums[1]);
+  console.log(descInts[1]);
 }
